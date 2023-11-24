9c3d34b472c8a88f610bf7dda847e029bcaf21da
Muhammad Rabeet khan <rkhan@nisum.com>
2023-11-24 19:14:25 +0500

    first commit

---
+++ src/main/java/com/example/practice/project/PracticeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practice/project/PracticeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
